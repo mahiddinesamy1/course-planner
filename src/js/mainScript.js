e5dede107b49e8116d2d4c293569044f514c78f6
PL1978 <philippe.lepage.3@ens.etsmtl.ca>
2023-01-26 17:13:18 -0500

    Progrès : erreur chargement JS dans page main NPM

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -1,8 +1,24 @@
+//Event listener
 window.addEventListener("DOMContentLoaded", function() {
     const courseForm = document.querySelector("form#fcourse");
     const courseFormSubmit = courseForm.querySelector("input#fsubmit");
     courseFormSubmit.onclick=function() {
-        console.log(courseForm.querySelector("input#fsigle").value);
+        courseInformationSubmitCallback(courseForm);
         return false;
     };
 });
+
+const iCalParser = require("./util/iCalParser");
+
+async function courseInformationSubmitCallback(form) {
+    let symbol = form.querySelector("input#fsymbol").value;
+    let year = form.querySelector("input#fyear").value;
+    let semester = form.querySelector("select#fsemester").value;
+    let group = form.querySelector("input#fgroup").value;
+    if (group.lenght == 1) {
+        group = "0" + group;
+    }
+    let parser = new iCalParser("C", symbol, group, year, semester);
+    await parser.parse();
+    console.log(parser);
+}
